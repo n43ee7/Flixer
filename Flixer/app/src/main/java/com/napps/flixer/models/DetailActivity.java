869077492aaabf3148628cb985873e57e433b6fe
n43ee7 <nabeel.nayyar6113@outlook.com>
2021-02-06 01:26:39 -0600

    AA- Stable

--- Flixer/app/src/main/java/com/napps/flixer/models/DetailActivity.java
+++ Flixer/app/src/main/java/com/napps/flixer/models/DetailActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
@@ -24,19 +25,18 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         // Setting Dynmaic UI Color
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_main);
-        @SuppressLint("CutPasteId") View someView = findViewById(R.id.MovieView);
+        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_detail);
+        @SuppressLint("CutPasteId") View someView = findViewById(R.id.linearLayout);
         View root = someView.getRootView();
         root.setBackgroundColor(getResources().getColor(R.color.UIblack));
-        @SuppressLint("CutPasteId") RecyclerView MoviesView = findViewById(R.id.MovieView);
+        @SuppressLint("CutPasteId") LinearLayout linearLayout = findViewById(R.id.linearLayout);
 
         MovieTitle = findViewById(R.id.DetailedMovieTitle);
         MovieOverview = findViewById(R.id.DetailedMovieOver);
         MovieRating = findViewById(R.id.MovieRating);
 
         Movie movie = Parcels.unwrap(getIntent().getParcelableExtra("movie"));
-        assert movie != null; // Assertion
         MovieTitle.setText(movie.getTitle());
         MovieOverview.setText(movie.getOverview());
         MovieRating.setRating((float) movie.getRating());
